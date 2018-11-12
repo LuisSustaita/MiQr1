fb4b92e216bba3f11935d0cf73b862b2f58c0176
LuisSustaita <ld.delacruzsustaita@ugto.mx>
2018-11-12 15:52:02 -0600

    Correccion de errores menores

--- test.py
+++ test.py
@@ -27,15 +27,14 @@ cambia_tamano_de_esta_imagen('imagenprueba.jpg')
 imagen1 = cv2.imread('qrprueba.png')
 imagen2 = cv2.imread('imagenprueba.jpg')
 
-imagen3 = imagen1[1:200, 1:200, :]
+#imagen3 = imagen1[1:200, 1:200, :]
 
 # fusion de imagenes
 # suma de imagenes, pero con diferentes pesos lo cual da una
 # sensacion de mezcla o transparencia
 # toma el 0.7 de la primer imagen y el 0.3 de la segunda imagen
-imagen_resultado = cv2.addWeighted(imagen3, 0.7, imagen2, 0.3, 0)
-# gamma es tomado como cero.
-pu
+imagen_resultado = cv2.addWeighted(imagen1, 0.7, imagen2, 0.3, 0)
+# gamma es tomado como cero
 
 cv2.imshow('imagen_resultado', imagen_resultado)
 cv2.imwrite('imagen_resultado.png', imagen_resultado)
