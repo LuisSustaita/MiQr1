3648b4d389db391d362c59e6f4068c41696354fc
LuisSustaita <ld.delacruzsustaita@ugto.mx>
2019-02-05 14:58:15 -0600

    Correccion de funciones de dithering

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ import cv2
 import qrcode
 
 
-def find_closest_palette_color(pixel):
-    return round(pixel/255)
+def find_new_pixel(pixel):
+    return 0 if pixel < 128 else 255
 
 
 # Floyd and Steinberg https://engineering.purdue.edu/~bouman/ece637/notes/pdf/Halftoning.pdf pag 22
@@ -16,16 +16,16 @@ def FloydDithering(imagen):
     for x in range(img_gray.shape[0]-1):
         for y in range(img_gray.shape[1]-1):
             oldpixel = img_gray[x, y]
-            newpixel = find_closest_palette_color(oldpixel)
+            newpixel = find_new_pixel(oldpixel)
             img_gray[x, y] = newpixel
             quant_error = oldpixel-newpixel
+            part = quant_error / 16
 
-            img_gray[x+1, y  ] = round(img_gray[x+1, y  ]+quant_error * 7/16)
-            img_gray[x-1, y+1] = round(img_gray[x-1, y+1]+quant_error * 3/16)
-            img_gray[x  , y+1] = round(img_gray[x  , y+1]+quant_error * 5/16)
-            img_gray[x+1, y+1] = round(img_gray[x+1, y+1]+quant_error * 1/16)
+            img_gray[x+1, y] = round(img_gray[x+1, y] + (part * 7))
+            img_gray[x-1, y+1] = round(img_gray[x-1, y+1] + (part * 3))
+            img_gray[x, y+1] = round(img_gray[x, y+1] + (part * 5))
+            img_gray[x+1, y+1] = round(img_gray[x+1, y+1] + (part * 1))
     cv2.imwrite('floydDit.jpg', img_gray)
-    binarizacion('floydDit.jpg', 23)
 
 
 # Jarvis, Judice, and Ninke https://engineering.purdue.edu/~bouman/ece637/notes/pdf/Halftoning.pdf pag 22
@@ -36,26 +36,27 @@ def JarvisDithering(imagen):
     for x in range(img_gray.shape[0] - 2):
         for y in range(img_gray.shape[1] - 2):
             oldpixel = img_gray[x, y]
-            newpixel = find_closest_palette_color(oldpixel)
+            newpixel = find_new_pixel(oldpixel)
             img_gray[x, y] = newpixel
             quant_error = oldpixel - newpixel
-            img_gray[x + 1, y    ] = round(img_gray[x + 1, y    ] + quant_error * 7 / 48)
-            img_gray[x + 2, y    ] = round(img_gray[x + 2, y    ] + quant_error * 5 / 48)
+            part = quant_error / 48
 
-            img_gray[x - 2, y + 1] = round(img_gray[x - 2, y + 1] + quant_error * 3 / 48)
-            img_gray[x - 1, y + 1] = round(img_gray[x - 1, y + 1] + quant_error * 5 / 48)
-            img_gray[x    , y + 1] = round(img_gray[x    , y + 1] + quant_error * 7 / 48)
-            img_gray[x + 1, y + 1] = round(img_gray[x + 1, y + 1] + quant_error * 5 / 48)
-            img_gray[x + 2, y + 1] = round(img_gray[x + 2, y + 1] + quant_error * 3 / 48)
+            img_gray[x + 1, y] = round(img_gray[x + 1, y] + part * 7)
+            img_gray[x + 2, y] = round(img_gray[x + 2, y] + part * 5)
 
-            img_gray[x - 2, y + 2] = round(img_gray[x - 2, y + 2] + quant_error * 1 / 48)
-            img_gray[x - 1, y + 2] = round(img_gray[x - 1, y + 2] + quant_error * 3 / 48)
-            img_gray[x    , y + 2] = round(img_gray[x    , y + 2] + quant_error * 5 / 48)
-            img_gray[x + 1, y + 2] = round(img_gray[x + 1, y + 2] + quant_error * 3 / 48)
-            img_gray[x + 2, y + 2] = round(img_gray[x + 2, y + 2] + quant_error * 1 / 48)
+            img_gray[x - 2, y + 1] = round(img_gray[x - 2, y + 1] + part * 3)
+            img_gray[x - 1, y + 1] = round(img_gray[x - 1, y + 1] + part * 5)
+            img_gray[x, y + 1] = round(img_gray[x, y + 1] + part * 7)
+            img_gray[x + 1, y + 1] = round(img_gray[x + 1, y + 1] + part * 5)
+            img_gray[x + 2, y + 1] = round(img_gray[x + 2, y + 1] + part * 3)
+
+            img_gray[x - 2, y + 2] = round(img_gray[x - 2, y + 2] + part * 1)
+            img_gray[x - 1, y + 2] = round(img_gray[x - 1, y + 2] + part * 3)
+            img_gray[x, y + 2] = round(img_gray[x, y + 2] + part * 5)
+            img_gray[x + 1, y + 2] = round(img_gray[x + 1, y + 2] + part * 3)
+            img_gray[x + 2, y + 2] = round(img_gray[x + 2, y + 2] + part * 1)
 
     cv2.imwrite('JarvisDit.jpg', img_gray)
-    binarizacion('JarvisDit.jpg', 31)
 
 
 def cambia_tamano_de_esta_imagen(nombre_imagen):
@@ -75,21 +76,17 @@ def binarizacion(imagen, threshold):
     img = cv2.imread(imagen)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    cv2.imwrite('gray.jpg', img_gray)
+    cv2.imwrite('graybin.jpg', img_gray)
 
     for i in range(img_gray.shape[0]):
         for j in range(img_gray.shape[1]):
+            print(" pixel {}".format(img_gray[i, j]))
             if img_gray[i, j] > threshold:
                 img_gray[i, j] = 0
             else:
                 img_gray[i, j] = 255
 
     cv2.imwrite('bin'+str(threshold)+'.jpg', img_gray)
-
-# def luminance_Beta(QR, imagen_original):
-# def luminance_Alfa(QR, imagen_original):
-# def luminance_BetaC(QR, imagen_original):
-# def luminance_AlfaC(QR, imagen_original):
 
 
 Qr = qrcode.make("Que tranza carranza")
@@ -101,8 +98,8 @@ cambia_tamano_de_esta_imagen('qrprueba.png')
 cambia_tamano_de_esta_imagen('carrito.png')
 
 # binarizacion('carrito.png', 127)
-# FloydDithering('carrito.png')
-JarvisDithering('carrito.png')
+FloydDithering('carrito.png')
+# JarvisDithering('carrito.png')
 
 imagen1 = cv2.imread('qrprueba.png')
 imagen2 = cv2.imread('carrito.png')
