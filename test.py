83048e5eabfee2e0f36bfdad679d4f949a7369a5
LuisSustaita <ld.delacruzsustaita@ugto.mx>
2019-04-02 14:27:18 -0600

    Se agrego modificacion para pasar a escala de grises

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import cv2
 # Importamos qrcode para generar Codigos QR propios
 import qrcode
+import numpy as np
 
 
 def find_new_pixel(pixel):
@@ -12,52 +13,68 @@ def find_new_pixel(pixel):
 def FloydDithering(imagen):
     img = cv2.imread(imagen)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    matrizimagen = np.zeros((200, 200))
 
     for x in range(img_gray.shape[0]-1):
         for y in range(img_gray.shape[1]-1):
             oldpixel = img_gray[x, y]
             newpixel = find_new_pixel(oldpixel)
-            img_gray[x, y] = newpixel
             quant_error = oldpixel-newpixel
+
             part = quant_error / 16
 
-            img_gray[x+1, y] = round(img_gray[x+1, y] + (part * 7))
-            img_gray[x-1, y+1] = round(img_gray[x-1, y+1] + (part * 3))
-            img_gray[x, y+1] = round(img_gray[x, y+1] + (part * 5))
-            img_gray[x+1, y+1] = round(img_gray[x+1, y+1] + (part * 1))
-    cv2.imwrite('floydDit.jpg', img_gray)
+            img_gray[x+1]  [y] = img_gray[x+1][y]   + (part * 7)
+            img_gray[x-1][y+1] = img_gray[x-1][y+1] + (part * 3)
+            img_gray[x]  [y+1] = img_gray[x]  [y+1] + (part * 5)
+            img_gray[x+1][y+1] = img_gray[x+1][y+1] + (part * 1)
+
+            matrizimagen[x][y] = newpixel
+
+    cv2.imwrite('floydDit.jpg', matrizimagen)
 
 
 # Jarvis, Judice, and Ninke https://engineering.purdue.edu/~bouman/ece637/notes/pdf/Halftoning.pdf pag 22
 def JarvisDithering(imagen):
     img = cv2.imread(imagen)
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    for x in range(img_gray.shape[0] - 2):
-        for y in range(img_gray.shape[1] - 2):
-            oldpixel = img_gray[x, y]
+    #img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    img_gray = []
+
+    for x in range(img.shape[0]):
+        img_gray.append([])
+        for y in range(img.shape[1]):
+            img_gray[x].append([])
+            img_gray[x][y] = (img[x, y, 0] * 0.2126) + (img[x, y, 1] * 0.7152) + (img[x, y, 2] * 0.0722)
+
+    matrizimagen = np.zeros((200, 200))
+
+    for x in range(len(img_gray) - 2):
+        for y in range(len(img_gray[x]) - 2):
+            oldpixel = img_gray[x][y]
             newpixel = find_new_pixel(oldpixel)
-            img_gray[x, y] = newpixel
             quant_error = oldpixel - newpixel
+
             part = quant_error / 48
 
-            img_gray[x + 1, y] = round(img_gray[x + 1, y] + part * 7)
-            img_gray[x + 2, y] = round(img_gray[x + 2, y] + part * 5)
+            img_gray[x + 1][ y] = round(img_gray[x + 1][ y] + part * 7)
+            img_gray[x + 2][ y] = round(img_gray[x + 2][ y] + part * 5)
 
-            img_gray[x - 2, y + 1] = round(img_gray[x - 2, y + 1] + part * 3)
-            img_gray[x - 1, y + 1] = round(img_gray[x - 1, y + 1] + part * 5)
-            img_gray[x, y + 1] = round(img_gray[x, y + 1] + part * 7)
-            img_gray[x + 1, y + 1] = round(img_gray[x + 1, y + 1] + part * 5)
-            img_gray[x + 2, y + 1] = round(img_gray[x + 2, y + 1] + part * 3)
+            img_gray[x - 2][ y + 1] = round(img_gray[x - 2][ y + 1] + part * 3)
+            img_gray[x - 1][ y + 1] = round(img_gray[x - 1][ y + 1] + part * 5)
+            img_gray[x][ y + 1] = round(img_gray[x][ y + 1] + part * 7)
+            img_gray[x + 1][ y + 1] = round(img_gray[x + 1][ y + 1] + part * 5)
+            img_gray[x + 2][ y + 1] = round(img_gray[x + 2][ y + 1] + part * 3)
 
-            img_gray[x - 2, y + 2] = round(img_gray[x - 2, y + 2] + part * 1)
-            img_gray[x - 1, y + 2] = round(img_gray[x - 1, y + 2] + part * 3)
-            img_gray[x, y + 2] = round(img_gray[x, y + 2] + part * 5)
-            img_gray[x + 1, y + 2] = round(img_gray[x + 1, y + 2] + part * 3)
-            img_gray[x + 2, y + 2] = round(img_gray[x + 2, y + 2] + part * 1)
+            img_gray[x - 2][ y + 2] = round(img_gray[x - 2][ y + 2] + part * 1)
+            img_gray[x - 1][ y + 2] = round(img_gray[x - 1][ y + 2] + part * 3)
+            img_gray[x][ y + 2] = round(img_gray[x][ y + 2] + part * 5)
+            img_gray[x + 1][ y + 2] = round(img_gray[x + 1][ y + 2] + part * 3)
+            img_gray[x + 2][ y + 2] = round(img_gray[x + 2][ y + 2] + part * 1)
 
-    cv2.imwrite('JarvisDit.jpg', img_gray)
+            matrizimagen[x][y] = newpixel
 
+    cv2.imwrite('JarvisDit.jpg', matrizimagen)
 
 def cambia_tamano_de_esta_imagen(nombre_imagen):
     from PIL import Image
@@ -90,19 +107,20 @@ def binarizacion(imagen, threshold):
 
 
 Qr = qrcode.make("Que tranza carranza")
-imagen = open("qrprueba.png", "wb")
-Qr.save(imagen)
-imagen.close()
+qrimagen = open("qrprueba.png", "wb")
+Qr.save(qrimagen)
+qrimagen.close()
+imagen = 'carrito.png'
 
 cambia_tamano_de_esta_imagen('qrprueba.png')
-cambia_tamano_de_esta_imagen('carrito.png')
+cambia_tamano_de_esta_imagen(imagen)
 
-# binarizacion('carrito.png', 127)
-FloydDithering('carrito.png')
-# JarvisDithering('carrito.png')
+# binarizacion(imagen, 127)
+# FloydDithering(imagen)
+JarvisDithering(imagen)
 
 imagen1 = cv2.imread('qrprueba.png')
-imagen2 = cv2.imread('carrito.png')
+imagen2 = cv2.imread(imagen)
 
 # fusion de imagenes
 # suma de imagenes, pero con diferentes pesos lo cual da una
@@ -118,9 +136,21 @@ cv2.destroyAllWindows()
 
 
 # Links de posible ayuda
+# ARTICULO BASE
+# file:///C:/Users/luisd/Downloads/DoubleColumn.pdf
+# 26 28 30
+# Presentacion basica
 # https://engineering.purdue.edu/~bouman/ece637/notes/pdf/Halftoning.pdf
+# Presentacion no tan basica
 # http://webstaff.itn.liu.se/~sasgo26/Dig_Halftoning/Lectures/Lecture_2013_1.pdf
+# Tipos de esparcimiento
 # https://blog.demofox.org/2017/10/20/generating-blue-noise-sample-points-with-mitchells-best-candidate-algorithm/
+# Libro de Halftoning
 # file:///C:/Users/luisd/Downloads/epdf.tips_modern-digital-halftoning-second-edition-signal-pr.pdf
+# Articulo medio pedorro de halftoning
 # https://pdfs.semanticscholar.org/7f8f/9fed14f0bd509af776a90cb220f9c26ccb81.pdf
+# TESIS Advanced halftoning methods
 # file:///C:/Users/luisd/Downloads/BPTX_2008_2_11320_0_233062_0_73868.pdf
+# Articulo A Simple and Efficient Error-Diffusion Algorithm
+# http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.16.8947&rep=rep1&type=pdf
+#
